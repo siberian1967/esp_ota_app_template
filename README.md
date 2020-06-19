b704eafece32a05ad9385fea0a76dc4448af949a
John Armstrong <john@brightandshiny.com>
2020-06-19 15:21:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# esp_ota_app_template
---
+++ zz_main_app.ino
@@ -0,0 +1,12 @@
+void setup(){
+  debug_setup();
+//  if (Debug.isActive(Debug.INFO)) {
+//    Debug.printf("bla bla bla: %d %s", 55, "Exciting"); // OR
+//  } else {
+//    Serial.println("BORING");
+//  }
+}
+
+void loop(){
+  debug_loop();
+}
